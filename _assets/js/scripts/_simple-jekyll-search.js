03ad04ac83724fd745ffb549f9ed74a76423bada
zachpanz88 <zachary@panzarino.com>
2015-12-08 19:38:29 -0500

    Remove unused assets

--- _assets/js/scripts/_simple-jekyll-search.js
+++
@@ -1,7 +0,0 @@
-var SimpleJekyllSearch = SimpleJekyllSearch || [];
-
-SimpleJekyllSearch({
-  searchInput: document.getElementById('search-input'),
-  resultsContainer: document.getElementById('results-container'),
-  json: baseurl + '/search.json'
-});
